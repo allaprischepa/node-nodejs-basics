2654ed3518c29cd4de374c36c9343b4b96e026e5
Alla Prishchepa <tyurina.alla@gmail.com>
2024-10-02 21:54:00 +0400

    feat: implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import * as fs from 'fs/promises';
+import path from 'path';
+
+const filePath = path.join(import.meta.dirname, 'files', 'fileToRead.txt');
+const readOptions = {
+  encoding: 'utf8',
+};
+
 const read = async () => {
-    // Write your code here 
+  fs.readFile(filePath, readOptions)
+    .then((res) => console.log(res))
+    .catch((err) => {
+      throw Error('FS operation failed', { cause: err });
+    });
 };
 
 await read();
