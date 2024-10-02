e774e7ace4e6adb79aa93789e39ef99dfbb1277d
Alla Prishchepa <tyurina.alla@gmail.com>
2024-10-02 14:21:10 +0400

    feat: implement delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,13 @@
+import * as fs from 'fs/promises';
+import path from 'path';
+
+const filePath = path.join(import.meta.dirname, 'files', 'fileToRemove.txt');
+
 const remove = async () => {
-    // Write your code here 
+  fs.unlink(filePath)
+    .catch((err) => {
+      throw Error('FS operation failed', { cause: err });
+    })
 };
 
 await remove();
