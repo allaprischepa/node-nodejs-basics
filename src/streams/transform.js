f3b720ad2c1fe413c8de2b2051e13e0a3b3c31e1
Alla Prishchepa <tyurina.alla@gmail.com>
2024-10-03 19:04:00 +0400

    feat: implement streams functionality

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,25 @@
+import { stdin, stdout } from 'process';
+import { pipeline, Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+  const tStream = new Transform({
+    transform(chunk, encoding, callback) {
+      const reversedChunk = chunk.toString().trimEnd().split('').reverse().join('');
+      this.push(`${reversedChunk}\n`);
+      callback();
+    },
+  });
+
+  stdout.write('Please, input text to reverse:\n');
+
+  pipeline(
+    stdin,
+    tStream,
+    stdout,
+    (err) => {
+      if (err) console.error(err);
+    }
+  );
 };
 
 await transform();
