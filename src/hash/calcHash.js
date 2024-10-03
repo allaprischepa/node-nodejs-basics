700e6cd6281b72493680c205d3ce89ca79f45355
Alla Prishchepa <tyurina.alla@gmail.com>
2024-10-03 13:13:49 +0400

    feat: implement hash functionality

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
+import path from 'path';
+import { createReadStream } from 'node:fs';
+const { createHash } = await import('node:crypto');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = path.join(import.meta.dirname, 'files', 'fileToCalculateHashFor.txt');
+  const hash = createHash('sha256');
+  const stream = createReadStream(filePath);
+
+  stream.on('error', (err) => console.error(err));
+  stream.on('data', (chunk) => hash.update(chunk));
+  stream.on('end', () => console.log(hash.digest('hex')));
 };
 
 await calculateHash();
