833d64fcbd76ad8ed3a4eb7ff334918661753676
Alla Prishchepa <tyurina.alla@gmail.com>
2024-10-04 09:35:09 +0400

    feat: implement zip functionality

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+import path from 'node:path';
+
 const compress = async () => {
-    // Write your code here 
+  const sourcePath = path.join(import.meta.dirname, 'files', 'fileToCompress.txt');
+  const destPath = path.join(import.meta.dirname, 'files', 'archive.gz');
+  const gzip = createGzip();
+  const source = createReadStream(sourcePath);
+  const destination = createWriteStream(destPath);
+
+  pipeline(
+    source,
+    gzip,
+    destination,
+    (err) => {
+      if (err) {
+        console.error(err);
+        process.exitCode = 1;
+      }
+    }
+  );
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
+import { createUnzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+import path from 'node:path';
+
 const decompress = async () => {
-    // Write your code here 
+  const sourcePath = path.join(import.meta.dirname, 'files', 'archive.gz');
+  const destPath = path.join(import.meta.dirname, 'files', 'fileToCompress.txt');
+  const unzip = createUnzip();
+  const source = createReadStream(sourcePath);
+  const destination = createWriteStream(destPath);
+
+  pipeline(
+    source,
+    unzip,
+    destination,
+    (err) => {
+      if (err) {
+        console.error(err);
+        process.exitCode = 1;
+      }
+    }
+  );
 };
 
 await decompress();
