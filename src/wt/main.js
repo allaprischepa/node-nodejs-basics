53cb48909abc985483299cc2f9f95e57810e9fa6
Alla Prishchepa <tyurina.alla@gmail.com>
2024-10-04 12:17:03 +0400

    feat: implement workers functionality

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,29 @@
+import path from 'path';
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+
+const createWorker = (data) => {
+  return new Promise((resolve, reject) => {
+    const workerPath = path.join(import.meta.dirname, 'worker.js');
+    const worker = new Worker(workerPath, { workerData: data });
+
+    worker.on('message', (data) => resolve({ status: 'resolved', data }));
+    worker.on('error', (err) => resolve({ status: 'error', data: null }));
+  });
+}
+
 const performCalculations = async () => {
-    // Write your code here
+  const cpusCount = cpus().length;
+  const startN = 10;
+  const workers = [];
+
+  for (let i = 0; i < cpusCount; i++) {
+    workers.push(createWorker(startN + i));
+  }
+
+  const res = await Promise.all(workers);
+
+  console.log(res);
 };
 
 await performCalculations();
